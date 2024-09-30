aad995748e2ef652f60af9719516623c23707391
Priyanshi-sahu <priyanshi.sahu15@gmail.com>
2024-09-30 15:40:34 +0530

    first commit

--- program2.js
+++ program2.js
@@ -4,6 +4,37 @@
  */
 var romanToInt = function(s) {
     
+    const romanNo = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+    
+    
+    let total = 0
+    
+  
+    for (let i = 0; i < s.length; i++) {
+    
+        const currentValue = romanNo[s[i]]
+    
+        const nextValue = romanNo[s[i + 1]]
+        
+      
+        if (nextValue > currentValue) {
+            total -= currentValue
+        } else {
+           
+            total += currentValue
+        }
+    }
+    
+   
+    return total;
 };
 
 
